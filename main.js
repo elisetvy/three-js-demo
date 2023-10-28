a1588c29e122a04b3a1f093fcc55bb7eb2477de6
Elise Ngo <elisetvy@gmail.com>
2023-10-28 16:40:39 -0400

    Make background transparent

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.querySelector("#app").appendChild( renderer.domElement );
 
-const texture = new THREE.TextureLoader().load('Screenshot 2023-10-27 at 22.06.15.png');
+// const texture = new THREE.TextureLoader().load('Screenshot 2023-09-22 at 17.18.27.png');
 
 const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( { map: texture } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
