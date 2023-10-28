cc8ce38753402288db755060ead0a325b1ef3f87
Elise Ngo <elisetvy@gmail.com>
2023-10-27 22:58:03 -0400

    Add animation and image mapping

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-import './style.css'
+import './style.css';
+
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerHeight, window.innerHeight);
-camera.position.setZ(30);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.querySelector("#app").appendChild( renderer.domElement );
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6349, wireframe: true });
-const torus = new THREE.Mesh(geometry, material);
+const texture = new THREE.TextureLoader().load('Screenshot 2023-10-27 at 22.06.15.png');
 
-scene.add(torus);
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const material = new THREE.MeshBasicMaterial( { map: texture } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+// const light = new THREE.AmbientLight( 0x404040, 75 ); // soft white light
+// scene.add( light );
+
+camera.position.z = 5;
 
 function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+	requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
 }
 
 animate();
