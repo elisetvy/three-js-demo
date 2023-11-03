b20360b7ddcaf6e8d7eae9d43cb227a395821acd
Elise Ngo <elisetvy@gmail.com>
2023-11-03 11:16:29 -0700

    Update demo with 3 examples

--- main.js
+++ main.js
@@ -5,27 +5,32 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer({ alpha: true });
+const renderer = new THREE.WebGLRenderer(); // { alpha: true }
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.querySelector("#app").appendChild( renderer.domElement );
+document.body.appendChild( renderer.domElement );
 
-// const texture = new THREE.TextureLoader().load('Screenshot 2023-09-22 at 17.18.27.png');
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.SphereGeometry();
+// const texture = new THREE.TextureLoader().load('texture.jpg');
+// const material = new THREE.MeshBasicMaterial({ map: texture });
 
-// const light = new THREE.AmbientLight( 0x404040, 75 ); // soft white light
-// scene.add( light );
+// const geometry = new THREE.TorusGeometry();
+// const texture = new THREE.TextureLoader().load('texture2.jpeg');
+// const material = new THREE.MeshBasicMaterial({ map: texture });
+
+const object = new THREE.Mesh( geometry, material );
+scene.add( object );
 
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
+	// requestAnimationFrame( animate );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // object.rotation.x += 0.01;
+  // object.rotation.y += 0.01;
+  // object.rotation.z += 0.01;
 
 	renderer.render( scene, camera );
 }
